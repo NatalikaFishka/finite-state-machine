cd6eb4cd619c74c89ad5563830510a4d4a96ebea
Natalya Okuneva <okuneva_natasha@mail.ru>
2019-10-07 17:42:54 +0300

    feat: FSM class

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,66 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+        this.state = config.initial;
+        this.stateRecord = ["normal"];
+        this.recordUndoneState = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (this.config.states.hasOwnProperty(state)) {
+            this.state = state;
+            if (this.state === "normal") {
+                this.stateRecord = ["normal"];
+            } else {
+                this.stateRecord.push(this.state);
+            }
+
+        } else {
+            throw new Error;
+        }
+
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (this.config.states[this.state].transitions[event]) {
+            this.state = this.config.states[this.state].transitions[event];
+            if (this.state === "normal") {
+                this.stateRecord = ["normal"];
+            } else {
+                this.stateRecord.push(this.state);
+            }
+            this.recordUndoneState = [];
+            return this.state;
+        } else {
+            throw new Error;
+        }
+
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +70,66 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let states = [];
+        let i;
+
+        if (event === undefined) {
+            for (i in this.config.states) {
+                states.push(i)
+            }
+        } else {
+            for (i in this.config.states) {
+                if (this.config.states[i].transitions[event]) {
+                    states.push(i)
+                }
+            }
+        }
+
+        return states;
+    }
+
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.stateRecord.length > 1) {
+            this.recordUndoneState.push(this.stateRecord.pop());
+            this.state = this.stateRecord[this.stateRecord.length - 1]
+            return true;
+        } else {
+            this.state = this.stateRecord[0];
+            console.log(this.state);
+            return false;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.recordUndoneState.length > 0) {
+            this.state = this.recordUndoneState[this.recordUndoneState.length - 1];
+            this.stateRecord.push(this.recordUndoneState.pop());
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.stateRecord = ["normal"];
+        this.recordUndoneState = [];
+    }
 }
 
 module.exports = FSM;
